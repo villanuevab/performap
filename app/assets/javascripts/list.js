47b0ba98f1707858c713017e9a88e3984a3b473b
Sophie Ye <sophixer@gmail.com>
2017-08-29 23:31:08 -0700

    implemented creation of table of cities and events

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -14,13 +14,15 @@ var ready = function (fn) {
   document.addEventListener('DOMContentLoaded', fn, false);
 };
 
-// retrive event details
+
+// retrieve event details
 var getEventById = function(event_id) {
   for (var i = 0, len = gon.events.length; i < len; i++) {
     if (gon.events[i].id === event_id)
       return gon.events[i];
   }
 }
+
 
 // expand directory to show list of countries and tags
 var showDirectory = function() {
@@ -31,6 +33,7 @@ var showDirectory = function() {
 var hideDirectory = function() {
   $('.directory-entries').slideUp('slow');
 };
+
 
 // show latest column of most recently updated events
 var showLatestColumn = function() {
@@ -46,14 +49,158 @@ var hideLatestColumn = function() {
   });
 };
 
+
+// show cities column
+var showCitiesColumn = function() {
+  document.querySelector('.col-cities').classList.remove('hidden');
+  $('.cities').slideDown('slow');
+};
+
+// hide cities column
+var hideCitiesColumn = function() {
+  document.querySelector('.cities').dataset.country = '';
+  $('.cities').slideUp('slow', function() {
+    document.querySelector('.col-cities').classList.add('hidden');
+  });
+};
+
+// setup secondary table for city > events
+var setupEventsTable = function() {
+  var table = document.createElement('table');
+  table.classList.add('secondary-table');
+  table.classList.add('tablesorter');
+
+  var div = document.createElement('div');
+  div.classList.add('secondary-table-wrapper');
+
+  var td = document.createElement('td');
+
+  var tr = document.createElement('tr');
+  tr.classList.add('tablesorter-childRow');
+  tr.classList.add('secondary-table-row');
+
+  div.appendChild(table);
+  td.appendChild(div);
+  tr.appendChild(td);
+
+  return tr;
+};
+
+// create thead for city > events table with name, venue, date
+var createTheadForEventsTable = function() {
+  var thead = document.createElement('thead');
+  var tr = document.createElement('tr');
+  var column_titles = ["Name", "Venue", "Date"];
+  for (var i = 0, len = column_titles.length; i < len; i++) {
+    var th = document.createElement('th');
+    th.textContent = column_titles[i];
+    tr.appendChild(th);
+  }
+  thead.appendChild(tr);
+  return thead;
+}
+
+// create tr for event with name, venue, date
+var createTrForEvent = function(event) {
+  var tr = document.createElement('tr');
+  tr.classList.add('event-entry');
+  tr.classList.add('link-details');
+  tr.dataset.eventId = event.id;
+
+  var td_name = document.createElement('td');
+  td_name.classList.add('event-entry-name');
+  td_name.textContent = event.name;
+  tr.appendChild(td_name);
+
+  var td_venue = document.createElement('td');
+  td_venue.classList.add('event-entry-venue');
+  td_venue.textContent = event.presenter;
+  tr.appendChild(td_venue)
+
+  var td_date = document.createElement('td');
+  td_date.classList.add('event-entry-start-date');
+  td_date.textContent = event.start_date;
+  tr.appendChild(td_date);
+
+  return tr;
+};
+
+// generate tbody for city > events table
+var createTbodyForEventsTable = function(city) {
+  var tbody = document.createElement('tbody');
+
+  var eventsForCity = Array.prototype.filter.call(gon.events, function(event) {
+    return event.city === city;
+  });
+
+  for (var i = 0, len = eventsForCity.length; i < len; i++)
+    tbody.appendChild(createTrForEvent(eventsForCity[i]));
+
+  return tbody;
+};
+
+// create secondary table of events for city
+var createEventsTableForCity = function(city) {
+  var fragment = document.createDocumentFragment();
+
+  fragment.appendChild(setupEventsTable());
+  fragment.querySelector('.secondary-table').appendChild(createTheadForEventsTable());
+  fragment.querySelector('.secondary-table').appendChild(createTbodyForEventsTable(city));
+
+  return fragment;
+};
+
+// create nested tr > td > a element for city entry in table
+var createTrForCity = function(city) {
+  var tr = document.createElement('tr');
+  tr.classList.add('city-entry');
+  var td = document.createElement('td');
+  var a = document.createElement('a');
+  a.setAttribute('href', '#');
+  a.dataset.city = city;
+  a.classList.add('toggle');
+  a.textContent = city;
+
+  td.appendChild(a);
+  tr.appendChild(td);
+
+  return tr;
+};
+
+// update table for cities and events
+var updateCitiesColumn = function(country) {
+  document.querySelector('#cities-table thead th').textContent = country;
+  document.querySelector('.cities').dataset.country = country;
+
+  var fragment = document.createDocumentFragment();
+  var tbody = document.createElement('tbody');
+
+  var cities = gon.citiesByCountry[country];
+  for (var i = 0, len = cities.length; i < len; i++) {
+    tbody.appendChild(createTrForCity(cities[i]));
+    tbody.appendChild(createEventsTableForCity(cities[i]));
+  }
+
+  fragment.appendChild(tbody);
+
+  var original_tbody = document.querySelector('#cities-table tbody');
+  document.querySelector('#cities-table').replaceChild(fragment, original_tbody);
+
+  $('#cities-table').trigger('update');
+  $('.tablesorter-childRow td .secondary-table-wrapper').hide();
+  $('.tablesorter-childRow > td').hide();
+}
+
+
 // show details column
 var showDetailsColumn = function() {
   document.querySelector('.col-details').classList.remove('hidden');
   $('.details').slideDown('slow');
-}
+};
 
 // hide details column
 var hideDetailsColumn = function() {
+  document.querySelector('.details').dataset.eventId = -1;
   $('.details').slideUp('slow', function() {
     document.querySelector('.col-details').classList.add('hidden');
   });
@@ -68,11 +215,12 @@ var updateDetailsColumn = function(event_id) {
   document.querySelector('.details-entry').textContent = event.description;
 }
 
+
 // restore page to original collapsed state by hiding columns
 var resetColumns = function() {
   hideDirectory();
   hideLatestColumn();
-  // hideCitiesColumn();
+  hideCitiesColumn();
   hideDetailsColumn();
 
   // remove any selected cell highlighting
@@ -81,10 +229,44 @@ var resetColumns = function() {
     selected_cells[i].classList.remove('selected');
 };
 
+
+var handleDirectoryLink = function(e) {
+  e.preventDefault();
+  if (document.querySelector('.col-latest').classList.contains('hidden')) {
+    showLatestColumn();
+    showDirectory();
+  } else
+    resetColumns();
+};
+
+var handleDirectoryCountryLink = function(elem) {
+  var country = elem.dataset.country;
+
+  // hide cities column if country is already displayed
+  if (document.querySelector('.cities').dataset.country === country) {
+    elem.classList.remove('selected');
+    hideCitiesColumn();
+    return false;
+  }
+
+  // update styling for selected cell
+  if (document.querySelector('.link-country.selected'))
+    document.querySelector('.link-country.selected').classList.remove('selected');
+  elem.classList.add('selected');
+
+  // update cities column
+  updateCitiesColumn(country);
+
+  // show cities column if hidden
+  if (document.querySelector('.col-cities.hidden'))
+    showCitiesColumn();
+};
+
+
 // returns true if elem clicked is link to event from latest column
 var isLatestEventLink = function(elem) {
   return elem.classList.contains('latest-entry') || elem.classList.contains('latest-entry-name') || elem.classList.contains('latest-entry-city');
-}
+};
 
 // toggle and update details for event clicked from latest column
 var handleLatestEventLink = function(elem) {
@@ -106,20 +288,27 @@ var handleLatestEventLink = function(elem) {
   // update event details
   updateDetailsColumn(event_id);
 
+  // TODO: collapse cities column
+
   // show details column if hidden
   if (document.querySelector('.col-details').classList.contains('hidden'))
     showDetailsColumn();
-}
+};
 
+// set up same listeners on multiple elements
 var linkHandler = function (e) {
   var elem = e.target;
   console.log(e);
 
-  // show details from event clicked from latest column
-  if (isLatestEventLink(elem)) {
+  if (elem.classList.contains('link-country')) {
+    // open cities column from clicking on country
+    handleDirectoryCountryLink(elem);
+  } else if (isLatestEventLink(elem)) {
+    // show details from event clicked from latest column
     handleLatestEventLink(elem);
   }
 };
+
 
 ready(function() {
   if (!supports)
@@ -128,14 +317,24 @@ ready(function() {
   document.addEventListener('click', linkHandler, false);
 
   // toggle directory menu and latest column
-  var directory_link = document.querySelector('a.link-latest');
-  directory_link.addEventListener('click', function(e) {
-    e.preventDefault();
-    if (document.querySelector('.col-latest').classList.contains('hidden')) {
-      showLatestColumn();
-      showDirectory();
-    } else {
-      resetColumns();
-    }
+  document.querySelector('a.link-latest').addEventListener('click', handleDirectoryLink, false);
+
+  // set up tablesorter
+  $('#cities-table').tablesorter({
+    cssChildRow: 'tablesorter-childRow'});
+  $('.tablesorter').delegate('.toggle', 'click', function() {
+    $(this).parent().toggleClass('selected');
+    $(this).closest('tr').nextUntil('tr:not(.tablesorter-childRow)').each(function() {
+      if ($(this).children('td').css('display') === 'none') {
+        $(this).children('td').show();
+        $(this).find('td .secondary-table-wrapper').slideDown('slow');
+        return $(this).find('table').tablesorter();
+      } else {
+        return $(this).find('td .secondary-table-wrapper').slideUp('slow', function() {
+          return $(this).parent().hide();
+        });
+      }
+    });
+    return false;
   });
 });
