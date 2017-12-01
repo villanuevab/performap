dfc9a954dd9279491907ddb6d0dd0ca21bda63a9
Sophie Ye <sophixer@gmail.com>
2017-12-01 21:43:43 +0100

    display primary city for latest event

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,13 +4,25 @@ class Event < ApplicationRecord
   scope :cities, -> { includes(:venues).pluck('venues.city').uniq }
   scope :recent, -> { order(updated_at: :desc) }
 
-  # get array of cities for given event based on associated venues
+  # get array of unique cities for given event based on associated venues
   def cities
     self.venues.pluck(:city).uniq
   end
 
-  # get array of countries for given event based on associated venues
+  # get the city with the majority of an event's venue(s)
+  def primary_city
+    mode(self.venues.pluck(:city))
+  end
+
+  # get array of unique countries for given event based on associated venues
   def countries
     self.venues.pluck(:country).uniq
   end
+
+  private
+    def mode(array)
+      freq = array.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+      array.max_by { |v| freq[v] }
+    end
+
 end
