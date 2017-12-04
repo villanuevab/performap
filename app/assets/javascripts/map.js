4f02509bb72025459c8c080f6f720f921d7c2617
Sophie Ye <sophixer@gmail.com>
2017-12-04 15:49:08 +0100

    basic markers working

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,8 +1,8 @@
 var map;
 function initMapModus() {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: new google.maps.LatLng(37.4274745,-122.1719077),
+    zoom: 4,
+    center: new google.maps.LatLng(39.8097343, -98.5556199),
     mapTypeControl: false
   });
 
@@ -13,6 +13,14 @@ var createMarkers = function() {
   console.log("creating markers...");
   var events = gon.events.results;
   for (var i = 0; i < events.length; i++) {
-    console.log(events[i].coordinates);
+    for (var j = 0; j < events[i].coordinates.length; j++) {
+      var coords = events[i].coordinates[j];
+      console.log(coords);
+      var latLng = new google.maps.LatLng(coords[0], coords[1]);
+      var marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+      })
+    }
   }
 };
